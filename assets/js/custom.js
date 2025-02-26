8f5068b7eda03f3fd91e7da3952e4de5a0cef8a4
RayCC51 <4q26dd@gmail.com>
2025-02-26 12:56:57 +0900

    fix: prevent log toc

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,9 +6,6 @@ const summary = document.querySelector('#table-of-contents > summary');
 const headings = document.querySelectorAll('h2, h3');
 const tocUl = document.querySelector("#table-of-contents > ul");
 
-let lastKnownScrollPosition = 0;
-let ticking = false;
-
 window.addEventListener('scroll', () => {
     // Table of contents, change summary defends on scroll position
     let current = '';
@@ -16,7 +13,7 @@ window.addEventListener('scroll', () => {
     headings.forEach(heading => {
         const headingTop = heading.getBoundingClientRect().top + window.scrollY;
         const headingHeight = heading.clientHeight;
-
+        
         if (window.scrollY >= headingTop - headingHeight / 2 - 150) {
             current = heading.textContent;
         }
